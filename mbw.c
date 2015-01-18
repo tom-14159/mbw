342777778f37959e36f77e7046025107979b748f
yyshen <yshen@gmx.com>
2015-01-18 19:36:37 +1100

    initial version on OSX

--- mbw.c
+++ mbw.c
@@ -55,8 +55,14 @@
 
 
 #ifdef __APPLE__
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <mach/thread_act.h>
 
 #define PTHREAD_BARRIER_SERIAL_THREAD   1
+#define SYSCTL_CORE_COUNT   "machdep.cpu.core_count"
+#define SYSCTL_THREAD_COUNT "machdep.cpu.thread_count"
+
 typedef struct cpu_set {
     uint32_t    count;
 } cpu_set_t;
@@ -69,25 +75,84 @@ static inline int
 CPU_ISSET(int num, cpu_set_t *cs) { return (cs->count & (1 << num)); }
 
 typedef struct pthread_barrier {
+    pthread_mutex_t         mutex;
+    pthread_cond_t          cond;
+    volatile uint32_t       flag; 
+    size_t                  count;
+    size_t                  num;
 } pthread_barrier_t;
 
 int pthread_barrier_init(pthread_barrier_t *bar, int attr, int num)
 {
+    
+    int ret = 0;
+    if ((ret = pthread_mutex_init(&(bar->mutex), 0))) return ret;
+    if ((ret = pthread_cond_init(&(bar->cond), 0))) return ret;
+    bar->flag = 0;
+    bar->count = 0;
+    bar->num = num;
+
     return 0;
 }
 
 int pthread_barrier_wait(pthread_barrier_t *bar)
 {
+    int ret = 0;
+    uint32_t flag = 0;
+
+    if ((ret = pthread_mutex_lock(&(bar->mutex)))) return ret;
+
+    flag = bar->flag;
+    bar->count++;
+
+    if (bar->count == bar->num) {
+        bar->count = 0;
+        bar->flag = 1 - bar->flag;
+        if ((ret = pthread_cond_broadcast(&(bar->cond)))) return ret;
+        if ((ret = pthread_mutex_unlock(&(bar->mutex)))) return ret;
+        return PTHREAD_BARRIER_SERIAL_THREAD;
+    }
+
+    while (1) {
+        if (bar->flag == flag) {
+            ret = pthread_cond_wait(&(bar->cond), &(bar->mutex));
+            if (ret) return ret;
+        } else { break; }
+    }
+
+    if ((ret = pthread_mutex_unlock(&(bar->mutex)))) return ret;
     return 0;
 }
 
 int sched_getaffinity(pid_t pid, size_t cpu_size, cpu_set_t *cpu_set)
 {
+    int32_t core_count = 0;
+    size_t  len = sizeof(core_count);
+    int ret = sysctlbyname(SYSCTL_CORE_COUNT, &core_count, &len, 0, 0);
+    if (ret) { 
+        printf("error while get core count %d\n", ret);
+        return -1;
+    }
+    cpu_set->count = 0;
+    for (int i = 0; i < core_count; i++) {
+        cpu_set->count |= (1 << i);
+    }
+
     return 0;
 }
 
 int pthread_setaffinity_np(pthread_t thread, size_t cpu_size, cpu_set_t *cpu_set)
 {
+    thread_port_t mach_thread;
+    int core = 0;
+
+    for (core = 0; core < 8 * cpu_size; core++) {
+        if (CPU_ISSET(core, cpu_set)) break;
+    }
+    printf("binding to core %d\n", core);
+    thread_affinity_policy_data_t policy = { core };
+    mach_thread = pthread_mach_thread_np(thread); 
+    thread_policy_set(mach_thread, THREAD_AFFINITY_POLICY, (thread_policy_t)&policy, 1);
     return 0;
 }
 
@@ -96,7 +161,6 @@ void *mempcpy(void *dest, const void *src, size_t size)
     memcpy(dest, src, size);
     return (dest + size);
 }
-
 
 #endif
 
@@ -113,7 +177,7 @@ static double mt = 0;
 /* number of threads */
 static int mt_num = 1;
 /* number of cores */
-static int num_cores = 1;
+static int num_cores = 0;
 static int core_factor = 1;
 /* fixed memcpy block size for -t2 */
 unsigned long long block_size=DEFAULT_BLOCK_SIZE;
